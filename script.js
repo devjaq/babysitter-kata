ffca56091b06f9f3e0aede744776b2360e420c46
devjaq <devjaq@gmail.com>
2019-05-05 18:52:24 -0400

    refactoring and changing  approach

--- script.js
+++ script.js
@@ -2,22 +2,87 @@
 
 // GOAL: calculate nightly charge for babysitter
 
+const A = [
+  { "time": "5:00", "charge": 15 },
+  { "time": "6:00", "charge": 15 },
+  { "time": "7:00", "charge": 15 },
+  { "time": "8:00", "charge": 15 },
+  { "time": "9:00", "charge": 15 },
+  { "time": "10:00", "charge": 15 },
+  { "time": "11:00", "charge": 20 },
+  { "time": "12:00", "charge": 20 },
+  { "time": "1:00", "charge": 20 },
+  { "time": "2:00", "charge": 20 },
+  { "time": "3:00", "charge": 20 },
+  { "time": "4:00", "charge": 20 },
+];
+
+const B = [
+  { "time": "5:00", "charge": 12 },
+  { "time": "6:00", "charge": 12 },
+  { "time": "7:00", "charge": 12 },
+  { "time": "8:00", "charge": 12 },
+  { "time": "9:00", "charge": 12 },
+  { "time": "10:00", "charge": 8 },
+  { "time": "11:00", "charge": 8 },
+  { "time": "12:00", "charge": 8 },
+  { "time": "1:00", "charge": 16 },
+  { "time": "2:00", "charge": 16 },
+  { "time": "3:00", "charge": 16 },
+  { "time": "4:00", "charge": 16 },
+];
+
+const C = [
+  { "time": "5:00", "charge": 21 },
+  { "time": "6:00", "charge": 21 },
+  { "time": "7:00", "charge": 21 },
+  { "time": "8:00", "charge": 21 },
+  { "time": "9:00", "charge": 15 },
+  { "time": "10:00", "charge": 15 },
+  { "time": "11:00", "charge": 15 },
+  { "time": "12:00", "charge": 15 },
+  { "time": "1:00", "charge": 15 },
+  { "time": "2:00", "charge": 15 },
+  { "time": "3:00", "charge": 15 },
+  { "time": "4:00", "charge": 15 },
+];
+
 // TODO: return correct total (first price-tier based) charge based on input of family and number of hours
 
-const calculateCharge = (family, hours) => {
-  if(family === 'A') {
-    return 15 * hours;
-  } else if(family === 'B') {
-    return 12 * hours;
-  } else if(family === 'C') {
-    return 21 * hours;
+let family;
+const calculateCharge = (familyInput, hours) => {
+  if (familyInput === 'A') {
+    family = A;
+  } else if (familyInput === 'B') {
+    family = B;
+  } else if (familyInput === 'C') {
+    family = C;
   }
-  return family;
+  return family[0].charge * hours;
 }
 
 // TODO: return hours worked
-const calculateHours = (start, end) => {
-  return end[0] - start[0];
+const calculateHours = (familyInput, start, end) => {
+  if (familyInput === 'A') {
+    family = A;
+  } else if (familyInput === 'B') {
+    family = B;
+  } else if (familyInput === 'C') {
+    family = C;
+  }
+  let hoursCounter = 0;
+  for (let i = 0; i < family.length; i++) {
+    if (family[i].time === start) {
+      let startTime = family[i].time;
+      for (let j = i; j < family.length; j++) {
+        if (family[j].time === end) {
+          return hoursCounter;
+        } else {
+          hoursCounter += 1;
+        }
+      }
+    }
+  }
 }
 
 module.exports = {
--- test.js
+++ test.js
@@ -3,28 +3,24 @@
 const test = require('./script.js');
 const assert = require('assert');
 
-
-it('should return 1', () => {
-  assert.equal(test.calculateCharge(1), 1);
-});
-
-it('should return input', () => {
-  assert.equal(test.calculateCharge(2), 2);
-});
-
-it('should return correct pre-bedtime hourly charge for family that is input', () => {
+it('should return correct hourly charge (based on 5:00) for the family that is input', () => {
   assert.equal(test.calculateCharge('A', 1), 15);
   assert.equal(test.calculateCharge('B', 1), 12);
   assert.equal(test.calculateCharge('C', 1), 21);
 });
 
-it('should calculate correct total (first price-tier based) charge based on input of family and number of hours', () => {
+it('should calculate correct total (based on 5:00) charge based on input of family and number of hours', () => {
   assert.equal(test.calculateCharge('A', 2), 30);
   assert.equal(test.calculateCharge('B', 4), 48);
   assert.equal(test.calculateCharge('C', 3), 63);
 });
 
-it('should calculate number of hours worked before 10:00', () => {
-  assert.equal(test.calculateHours('5:00', '9:00'), 4);
-  assert.equal(test.calculateHours('5:00', '6:00'), 1);
+it('should calculate number of hours worked', () => {
+  assert.equal(test.calculateHours('A', '5:00', '9:00'), 4);
+  assert.equal(test.calculateHours('B', '8:00', '2:00'), 6);
+});
+
+it('should calculate total charge', () => {
+  assert.equal(test.calculateHours('A', '5:00', '9:00'), 4);
+  assert.equal(test.calculateHours('B', '8:00', '2:00'), 6);
 });
