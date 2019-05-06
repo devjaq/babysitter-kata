7ec591d7c85889491bf186b08035b0567aa383a5
devjaq <devjaq@gmail.com>
2019-05-06 13:09:08 -0400

    refactor and account for AM/PM

--- script.js
+++ script.js
@@ -3,60 +3,55 @@
 // GOAL: calculate nightly charge for babysitter
 
 const A = [
-  { "time": "5:00", "charge": 15 },
-  { "time": "6:00", "charge": 15 },
-  { "time": "7:00", "charge": 15 },
-  { "time": "8:00", "charge": 15 },
-  { "time": "9:00", "charge": 15 },
-  { "time": "10:00", "charge": 15 },
-  { "time": "11:00", "charge": 20 },
-  { "time": "12:00", "charge": 20 },
-  { "time": "1:00", "charge": 20 },
-  { "time": "2:00", "charge": 20 },
-  { "time": "3:00", "charge": 20 },
-  { "time": "4:00", "charge": 20 },
+  { "time": "5:00 PM", "charge": 15 },
+  { "time": "6:00 PM", "charge": 15 },
+  { "time": "7:00 PM", "charge": 15 },
+  { "time": "8:00 PM", "charge": 15 },
+  { "time": "9:00 PM", "charge": 15 },
+  { "time": "10:00 PM", "charge": 15 },
+  { "time": "11:00 PM", "charge": 20 },
+  { "time": "12:00 AM", "charge": 20 },
+  { "time": "1:00 AM", "charge": 20 },
+  { "time": "2:00 AM", "charge": 20 },
+  { "time": "3:00 AM", "charge": 20 },
+  { "time": "4:00 AM", "charge": 20 },
 ];
 
 const B = [
-  { "time": "5:00", "charge": 12 },
-  { "time": "6:00", "charge": 12 },
-  { "time": "7:00", "charge": 12 },
-  { "time": "8:00", "charge": 12 },
-  { "time": "9:00", "charge": 12 },
-  { "time": "10:00", "charge": 8 },
-  { "time": "11:00", "charge": 8 },
-  { "time": "12:00", "charge": 8 },
-  { "time": "1:00", "charge": 16 },
-  { "time": "2:00", "charge": 16 },
-  { "time": "3:00", "charge": 16 },
-  { "time": "4:00", "charge": 16 },
+  { "time": "5:00 PM", "charge": 12 },
+  { "time": "6:00 PM", "charge": 12 },
+  { "time": "7:00 PM", "charge": 12 },
+  { "time": "8:00 PM", "charge": 12 },
+  { "time": "9:00 PM", "charge": 12 },
+  { "time": "10:00 PM", "charge": 8 },
+  { "time": "11:00 PM", "charge": 8 },
+  { "time": "12:00 AM", "charge": 8 },
+  { "time": "1:00 AM", "charge": 16 },
+  { "time": "2:00 AM", "charge": 16 },
+  { "time": "3:00 AM", "charge": 16 },
+  { "time": "4:00 AM", "charge": 16 },
 ];
 
 const C = [
-  { "time": "5:00", "charge": 21 },
-  { "time": "6:00", "charge": 21 },
-  { "time": "7:00", "charge": 21 },
-  { "time": "8:00", "charge": 21 },
-  { "time": "9:00", "charge": 15 },
-  { "time": "10:00", "charge": 15 },
-  { "time": "11:00", "charge": 15 },
-  { "time": "12:00", "charge": 15 },
-  { "time": "1:00", "charge": 15 },
-  { "time": "2:00", "charge": 15 },
-  { "time": "3:00", "charge": 15 },
-  { "time": "4:00", "charge": 15 },
+  { "time": "5:00 PM", "charge": 21 },
+  { "time": "6:00 PM", "charge": 21 },
+  { "time": "7:00 PM", "charge": 21 },
+  { "time": "8:00 PM", "charge": 21 },
+  { "time": "9:00 PM", "charge": 15 },
+  { "time": "10:00 PM", "charge": 15 },
+  { "time": "11:00 PM", "charge": 15 },
+  { "time": "12:00 AM", "charge": 15 },
+  { "time": "1:00 AM", "charge": 15 },
+  { "time": "2:00 AM", "charge": 15 },
+  { "time": "3:00 AM", "charge": 15 },
+  { "time": "4:00 AM", "charge": 15 },
 ];
+let family;
 
 // TODO: return charge for hours worked
 const calculateCharge = (familyInput, start, end) => {
-  let family;
-  if (familyInput === 'A') {
-    family = A;
-  } else if (familyInput === 'B') {
-    family = B;
-  } else if (familyInput === 'C') {
-    family = C;
-  }
+  family = processFamilyInput(familyInput);
+
   let totalCharge = 0;
   for (let i = 0; i < family.length; i++) {
     if (family[i].time === start) {
@@ -71,6 +66,17 @@ const calculateCharge = (familyInput, start, end) => {
     }
   }
 }
+
+const processFamilyInput = (familyInput) => {
+  if (familyInput === 'A') {
+    family = A;
+  } else if (familyInput === 'B') {
+    family = B;
+  } else if (familyInput === 'C') {
+    family = C;
+  }
+  return family;
+};
 
 module.exports = {
   calculateCharge
--- test.js
+++ test.js
@@ -5,6 +5,6 @@ const assert = require('assert');
 
 
 it('should calculate total charge', () => {
-  assert.equal(test.calculateCharge('A', '5:00', '9:00'), 60);
-  assert.equal(test.calculateCharge('B', '8:00', '2:00'), 64);
+  assert.equal(test.calculateCharge('A', '5:00 PM', '9:00 PM'), 60);
+  assert.equal(test.calculateCharge('B', '8:00 PM', '2:00 AM'), 64);
 });
